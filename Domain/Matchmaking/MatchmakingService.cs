5193a1dfb4a990bc6c34bebb0ad5577708847ab0
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-01-23 11:38:19 +0100

    dont automatically add back people on reset

--- Domain/Matchmaking/MatchmakingService.cs
+++ Domain/Matchmaking/MatchmakingService.cs
@@ -74,15 +74,6 @@ public class MatchmakingService(ApplicationDbContext db)
         var groups = await db.Groups.ToArrayAsync();
         db.RemoveRange(groups);
 
-        var people = new List<Person> {
-            new Person() { Id = Guid.NewGuid(), Name = "Van Hellsing", UserId = Guid.NewGuid(), Criteria = new Criteria() { Availability = Availability.Daytime, Language = "en" }, Preferences = new Preferences() { } },
-            new Person() { Id = Guid.NewGuid(), Name = "Blade", UserId = Guid.NewGuid(), Criteria = new Criteria() { Availability = Availability.Daytime, Language = "en" }, Preferences = new Preferences() { } },
-            new Person() { Id = Guid.NewGuid(), Name = "Nosferatu", UserId = Guid.NewGuid(), Criteria = new Criteria() { Availability = Availability.Afternoons, Language = "en" }, Preferences = new Preferences() { } },
-            new Person() { Id = Guid.NewGuid(), Name = "Dracula", UserId = Guid.NewGuid(), Criteria = new Criteria() { Availability = Availability.Afternoons, Language = "en" }, Preferences = new Preferences() { } },
-            new Person() { Id = Guid.NewGuid(), Name = "Sylvanas", UserId = Guid.NewGuid(), Criteria = new Criteria() { Availability = Availability.Afternoons, Language = "en" }, Preferences = new Preferences() { } }
-        };
-        db.AddRange(people);
-
         await db.SaveChangesAsync();
     }
 
