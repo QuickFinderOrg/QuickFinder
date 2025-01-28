f1eb4665c84c0dff1fce761339052a162bb8e742
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-01-28 12:09:01 +0100

    user group many-to-many

---
+++ Data/Migrations/20250128110529_UsersGroups.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace group_finder.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersGroups : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Groups_GroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_GroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "GroupUser",
+                columns: table => new
+                {
+                    GroupsId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    MembersId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupUser", x => new { x.GroupsId, x.MembersId });
+                    table.ForeignKey(
+                        name: "FK_GroupUser_AspNetUsers_MembersId",
+                        column: x => x.MembersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupUser_Groups_GroupsId",
+                        column: x => x.GroupsId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupUser_MembersId",
+                table: "GroupUser",
+                column: "MembersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GroupUser");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "GroupId",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_GroupId",
+                table: "AspNetUsers",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Groups_GroupId",
+                table: "AspNetUsers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+    }
+}
