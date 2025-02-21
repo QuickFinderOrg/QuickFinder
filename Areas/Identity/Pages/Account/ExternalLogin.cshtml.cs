8d4ae18459def5c0c1042e12701207884ab2e4dd
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-02-21 15:41:08 +0100

    remove test

--- Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -90,12 +90,9 @@ namespace group_finder.Areas.Identity.Pages.Account
 
         public IActionResult OnPost(string provider, string returnUrl = null)
         {
-            var domain = HttpContext.Request.Host.Value;
-            _logger.LogInformation("Current domain: {Domain}", domain);
             // Request a redirect to the external login provider.
             var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
             var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
-            properties.RedirectUri = $"https://example.com/{properties.RedirectUri}";
             return new ChallengeResult(provider, properties);
         }
 
