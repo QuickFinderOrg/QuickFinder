e0b51012d05b035aaf47c8c69562b5ba07ae68f7
MartinVorre <116595392+MartinVorre@users.noreply.github.com>
2025-02-21 11:32:29 +0100

    routes

---
+++ Routes.cs
@@ -0,0 +1,21 @@
+namespace group_finder;
+
+public static class StudentRoutes
+{
+    public static string Home() => "/Index";
+    public static string Groups() => "/Student/Groups";
+    public static string Matching() => "/Student/Matching";
+    public static string Preferences() => "/Student/Preferences";
+}
+
+public static class TeacherRoutes
+{
+    public static string CourseGroups() => "/Teacher/CourseGroups";
+    public static string EditGroup() => "/Teacher/EditGroup";
+}
+
+public static class AdminRoutes
+{
+    public static string AddStudent() => "/AddStudent";
+    public static string Students() => "/Students";
+}
