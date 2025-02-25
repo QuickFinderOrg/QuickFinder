3d7c4df7fc1ee2003ab12b942d2d34ae608e2347
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-02-25 12:37:26 +0100

    get discord token

--- Pages/discord-login.cshtml.cs
+++ Pages/discord-login.cshtml.cs
@@ -1,3 +1,7 @@
+using System.Configuration;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -36,13 +40,54 @@ namespace group_finder.Pages
         /// </summary>
         public static readonly string UserInformationEndpoint = "https://discord.com/api/users/@me";
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
             var url = HttpContext.Request.GetDisplayUrl();
+            var code = HttpContext.Request.Query["code"];
+            var state = HttpContext.Request.Query["state"]; //TODO: use state for redirecting
             Console.WriteLine(url);
+
+            if (Microsoft.Extensions.Primitives.StringValues.IsNullOrEmpty(code))
+            {
+                throw new Exception("Code is null");
+            }
+
+            var token = await ExchangeCodeAsync(code!);
+            Console.WriteLine($"Discord Token: {token}");
             return Redirect("/");
         }
 
+        public async Task<string> ExchangeCodeAsync(string code)
+        {
+            using var client = new HttpClient();
+            var data = new FormUrlEncodedContent(
+            [
+                new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                new KeyValuePair<string, string>("code", code),
+                new KeyValuePair<string, string>("redirect_uri", $"{Request.Scheme}://{Request.Host}{CallbackPath}")
+            ]);
 
+            var authValue = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{configuration[Constants.DiscordClientIdKey]}:{configuration[Constants.DiscordClientSecretKey]}"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authValue);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+
+            HttpResponseMessage response = await client.PostAsync(TokenEndpoint, data);
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+            response.EnsureSuccessStatusCode();
+
+            var token_json_string = await response.Content.ReadAsStringAsync();
+
+            var tokenData = System.Text.Json.JsonDocument.Parse(token_json_string);
+            var accessToken = tokenData.RootElement.GetProperty("access_token").GetString();
+
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                throw new Exception("access_token is null");
+            }
+
+            // TODO: store refresh token
+
+            return accessToken;
+        }
     }
 }
