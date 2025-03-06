8e671a1328ca061ddbfe3fabed7e19f8b8a8b299
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-06 15:33:34 +0100

    add smtp email sending test

---
+++ SMTPEmailSender.cs
@@ -0,0 +1,42 @@
+using System;
+
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+namespace group_finder;
+
+
+
+public class SMTPEmailSender(ILogger<StubEmailSender> logger) : IEmailSender
+{
+
+    public async Task SendEmailAsync(string email, string subject, string body)
+    {
+        logger.LogInformation($"MAILTO={email} SUBJECT=subject BODY={body}");
+
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Joey Tribbiani", "joey@friends.com"));
+        message.To.Add(new MailboxAddress("Mrs. Chanandler Bong", "chandler@friends.com"));
+        message.Subject = "How you doin'?";
+
+        message.Body = new TextPart("plain")
+        {
+            Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+        };
+
+        using (var client = new SmtpClient())
+        {
+            client.Connect("localhost", 25, false);
+
+            client.Send(message);
+            client.Disconnect(true);
+        }
+
+        await Task.CompletedTask;
+    }
+}
