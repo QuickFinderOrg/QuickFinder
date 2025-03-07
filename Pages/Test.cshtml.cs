9119b476bf8c9649e2fd188df224bc5ed7d5db3b
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-07 15:30:24 +0100

    only display test page in dev mode

--- Pages/Test.cshtml.cs
+++ Pages/Test.cshtml.cs
@@ -3,18 +3,28 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace group_finder.Pages;
 
-public class TestModel(ILogger<TestModel> logger, DiscordBotService discordBotService) : PageModel
+public class TestModel(ILogger<TestModel> logger, DiscordBotService discordBotService, IWebHostEnvironment environment) : PageModel
 {
     private readonly ILogger<TestModel> _logger = logger;
 
     public string TestResult = "";
 
-    public void OnGet()
+    public IActionResult OnGet()
     {
+        if (environment.IsDevelopment() == false)
+        {
+            return Redirect("/");
+        }
+
+        return Page();
     }
 
     public async Task<IActionResult> OnPostAsync()
     {
+        if (environment.IsDevelopment() == false)
+        {
+            return Redirect("/");
+        }
         _logger.LogInformation("POST: test");
         var channelId = await discordBotService.CreateChannel("channel-5");
         TestResult = $"channel {channelId}";
