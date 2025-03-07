ae6f5890f8a46bed9b5d090a1dd0491d77f4c3c1
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-07 11:07:06 +0100

    db migrate

---
+++ Data/Migrations/20250307100526_AddGroupIsComplete.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace group_finder.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGroupIsComplete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsComplete",
+                table: "Groups",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsComplete",
+                table: "Groups");
+        }
+    }
+}
