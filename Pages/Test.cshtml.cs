e91c46eb3f383a6bd38f532196e8e8b890d829ba
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-07 14:15:58 +0100

    add test page

---
+++ Pages/Test.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace group_finder.Pages;
+
+public class TestModel(ILogger<TestModel> logger) : PageModel
+{
+    private readonly ILogger<TestModel> _logger = logger;
+
+    public string TestResult = "";
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        _logger.LogInformation("POST: test");
+        TestResult = "Test success";
+        return Page();
+    }
+}
+
