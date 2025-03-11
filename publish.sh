ec7859de4149d485d04fcb170b292a4a7ac7ea32
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-11 15:34:00 +0100

    use tar to publish

--- publish.sh
+++ publish.sh
@@ -5,10 +5,8 @@ UNIX_SERVER="sturlen@gorina3.ux.uis.no"
 PROJECT_DIR="/mnt/c/projects/group-finder/"
 
 ssh -J $LOGIN_SERVER $UNIX_SERVER "tmux kill-session -t webserver"
-
 ssh -J $LOGIN_SERVER $UNIX_SERVER 'rm -rf /local/home/quickfinder/publish' &&
-scp -r -J sturlen@ssh4.ux.uis.no /mnt/c/projects/group-finder/publish/ sturlen@gorina3.ux.uis.no:/local/home/quickfinder/ &&
-scp -r -J sturlen@ssh4.ux.uis.no /mnt/c/projects/group-finder/appsettings.Production.json sturlen@gorina3.ux.uis.no:/local/home/quickfinder/publish
+tar -czf - publish/ | ssh -J $LOGIN_SERVER $UNIX_SERVER "tar -xzf - -C /local/home/quickfinder/"
 
 read -p "Do you want to start the webserver as well? (y/n): " webserver_answer
 if [ "$webserver_answer" = "y" ]; then
