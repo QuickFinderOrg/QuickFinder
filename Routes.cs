08a8ddce6e63bfb00356cbd79cd2c9c842920ce6
MartinVorre <116595392+MartinVorre@users.noreply.github.com>
2025-03-11 14:05:24 +0100

    Exchange code and create user

--- Routes.cs
+++ Routes.cs
@@ -3,6 +3,7 @@ namespace group_finder;
 public static class StudentRoutes
 {
     public static string Home() => "/Index";
+    public static string Login() => "/Login";
     public static string Groups() => "/Student/Groups";
     public static string Matching() => "/Student/Matching";
     public static string Preferences() => "/Student/Preferences";
--- UserExtensions.cs
+++ UserExtensions.cs
@@ -4,27 +4,27 @@ namespace group_finder;
 public static class UserExtensions
 {
 
-    public static void SetUserId(this HttpContext context, int userId)
+    public static void SetUserId(this HttpContext context, string userId)
     {
-        context.Session.SetInt32(Constants.UserSessionKey, userId);
+        context.Session.SetString(Constants.UserSessionKey, userId);
     }
 
-    public static int? GetUserId(this HttpContext context)
+    public static string? GetUserId(this HttpContext context)
     {
         var userId_override = context.RequestServices.GetRequiredService<IConfiguration>()["UserIdOverride"];
 
         if (!string.IsNullOrWhiteSpace(userId_override))
         {
             Console.WriteLine($"UserId override: {userId_override}");
-            return int.Parse(userId_override);
+            return userId_override;
         }
 
-        return context.Session.GetInt32(Constants.UserSessionKey);
+        return context.Session.GetString(Constants.UserSessionKey);
     }
 
-    public static int RequireUserId(this HttpContext context)
+    public static string RequireUserId(this HttpContext context)
     {
-        return context.Session.GetInt32(Constants.UserSessionKey) ?? throw new Exception("ID not found in session");
+        return context.Session.GetString(Constants.UserSessionKey) ?? throw new Exception("ID not found in session");
     }
 
     public static void ClearUserId(this HttpContext context)
