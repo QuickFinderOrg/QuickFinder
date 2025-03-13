dcb3414ae4db32e766d38b4cdcdaad52fc9d5b4c
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-13 15:10:51 +0100

    feat: log if bot recieves message

--- Domain/Discord/DiscordService.cs
+++ Domain/Discord/DiscordService.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 
 namespace group_finder;
 
-public class DiscordService(ulong serverId, ulong groupChannelId)
+public class DiscordService(ulong serverId, ulong groupChannelId, ILogger<DiscordService> logger)
 {
     private readonly DiscordSocketClient _client = new DiscordSocketClient();
 
@@ -13,6 +11,8 @@ public class DiscordService(ulong serverId, ulong groupChannelId)
     {
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
+
+        _client.MessageReceived += a => { logger.LogInformation("Discord bot recieved message {msg}", a.Content); return Task.CompletedTask; };
     }
 
     public async Task<bool> SendDM(ulong userId, string message)
