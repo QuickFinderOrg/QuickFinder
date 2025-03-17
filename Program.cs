de6d6220b4800c8b819551a0130a6e1a45c9667a
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-17 16:04:29 +0100

    always enable admin mode when in dev

--- Program.cs
+++ Program.cs
@@ -38,13 +38,21 @@ builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("Student", policy => policy.RequireAuthenticatedUser());
 
-    options.AddPolicy("Teacher", policy =>
-    policy.RequireAssertion(context =>
-        context.User.HasClaim(c =>
-            c.Type == "IsTeacher" ||
-            c.Type == "IsAdmin")));
+    if (builder.Environment.IsDevelopment())
+    {
+        options.AddPolicy("Teacher", policy => policy.RequireAuthenticatedUser());
+        options.AddPolicy("Admin", policy => policy.RequireAuthenticatedUser());
+    }
+    else
+    {
+        options.AddPolicy("Teacher", policy =>
+        policy.RequireAssertion(context =>
+            context.User.HasClaim(c =>
+                c.Type == "IsTeacher" ||
+                c.Type == "IsAdmin")));
 
-    options.AddPolicy("Admin", policy => policy.RequireClaim("IsAdmin"));
+        options.AddPolicy("Admin", policy => policy.RequireClaim("IsAdmin"));
+    }
 });
 
 builder.Services.ConfigureApplicationCookie(options => options.LoginPath = StudentRoutes.Login());
