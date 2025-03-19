af24ded8f639112a1983d4e7e77c7b99b0292d34
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-19 14:31:34 +0100

    add permission grant to test page

--- Pages/Test.cshtml.cs
+++ Pages/Test.cshtml.cs
@@ -1,10 +1,13 @@
 ﻿using group_finder.Domain.DiscordDomain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace group_finder.Pages;
 
-public class TestModel(ILogger<TestModel> logger, DiscordService discordBotService, IWebHostEnvironment environment) : PageModel
+[Authorize(Policy = "Admin")]
+public class TestModel(ILogger<TestModel> logger, DiscordService discordBotService, IWebHostEnvironment environment, UserService userService, UserManager<User> userManager) : PageModel
 {
     private readonly ILogger<TestModel> _logger = logger;
 
@@ -15,17 +18,6 @@ public class TestModel(ILogger<TestModel> logger, DiscordService discordBotServi
         return Page();
     }
 
-    public async Task<IActionResult> OnPostAsync()
-    {
-        if (environment.IsDevelopment() == false)
-        {
-            return Redirect("/");
-        }
-        _logger.LogInformation("POST: test");
-        // var channelId = await discordBotService.CreateChannel("channel-5");
-        // TestResult = $"channel {channelId}";
-        return RedirectToPage();
-    }
     public async Task<IActionResult> OnPostDeleteChannelAsync(string id)
     {
         if (environment.IsDevelopment() == false)
@@ -43,5 +35,42 @@ public class TestModel(ILogger<TestModel> logger, DiscordService discordBotServi
         TestResult = $"channel {channelId}";
         return RedirectToPage();
     }
+
+    public async Task<IActionResult> OnPostSetChannelPermissionsAsync(string id)
+    {
+        if (environment.IsDevelopment() == false)
+        {
+            return Redirect("/");
+        }
+
+        if (string.IsNullOrWhiteSpace("id"))
+        {
+            return RedirectToPage();
+        }
+
+        var user = await userManager.GetUserAsync(HttpContext.User);
+        if (user == null)
+        {
+            _logger.LogError("User not logged in");
+            return RedirectToPage();
+        }
+
+
+        var discord_user_id = await userService.GetDiscordId(user.Id);
+        if (discord_user_id == null)
+        {
+            _logger.LogError("Discord id not found");
+            return RedirectToPage();
+        }
+
+        _logger.LogInformation("POST: update permissions for {channelId}", id);
+        var channelId = await discordBotService.SetUserPermissionsOnChannel(ulong.Parse(id), (ulong)discord_user_id);
+        if (channelId == null)
+        {
+            _logger.LogError("Failed to set permsissions");
+        }
+        return RedirectToPage();
+    }
+
 }
 
