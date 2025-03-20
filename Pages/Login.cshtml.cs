0ccebd37960a63d013e1b5bd4f76a4283c050432
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-20 10:53:45 +0100

    ask to see servers on login

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -16,7 +16,7 @@ namespace group_finder.Pages
         {
             var redirect_uri_raw = $"https://{HttpContext.Request.Host}/LoginDiscord";
             var redirect_uri = System.Net.WebUtility.UrlEncode(redirect_uri_raw);
-            var discord_url = $"https://discord.com/oauth2/authorize?client_id=1328341129078505499&response_type=code&redirect_uri={redirect_uri}&scope=identify+email";
+            var discord_url = $"https://discord.com/oauth2/authorize?client_id=1328341129078505499&response_type=code&redirect_uri={redirect_uri}&scope=identify+email+guilds";
             return Redirect(discord_url);
         }
     }
