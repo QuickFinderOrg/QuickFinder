ef927b4684c18bf1e4416ad8c062cf20c6362d7a
MartinVorre <116595392+MartinVorre@users.noreply.github.com>
2025-03-25 13:55:38 +0100

    CreateGroup basic page

---
+++ Pages/Student/CreateGroup.cshtml.cs
@@ -0,0 +1,22 @@
+using group_finder.Domain.Matchmaking;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace group_finder.Pages.Student;
+
+public class CreateGroupModel(MatchmakingService matchmaking) : PageModel
+{
+    public Course? Course;
+    public Group[] Groups = [];
+    public async Task<IActionResult> OnGetAsync(Guid Id)
+    {
+        await LoadAsync(Id);
+        return Page();
+    }
+
+    public async Task LoadAsync(Guid Id)
+    {
+        Course = await matchmaking.GetCourse(Id);
+        Groups = await matchmaking.GetGroups(Id);
+    }
+}
