da46bb3352d3b181123d85f0c4bc1448e4879464
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-25 12:37:03 +0100

    basic discord server overview page

---
+++ Pages/Admin/Servers.cs
@@ -0,0 +1,27 @@
+using group_finder.Domain.DiscordDomain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace group_finder.Pages.Admin;
+
+public class ServersModel(ILogger<StudentsModel> logger, DiscordService discordService) : PageModel
+{
+    private readonly ILogger<StudentsModel> _logger = logger;
+
+    public DiscordServerItem[] Servers = [];
+    public string InviteURL = "";
+
+    [BindProperty]
+    public string SearchQuery { get; set; } = string.Empty;
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        Servers = await discordService.GetServerList();
+        InviteURL = discordService.InviteURL;
+        return Page();
+    }
+
+
+}
+
+
