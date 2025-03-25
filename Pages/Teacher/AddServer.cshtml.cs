d6f70b22d02805e95bb5644fd16014223cf1ac70
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-03-25 15:34:22 +0100

    test adding discord server to existing course

---
+++ Pages/Teacher/AddServer.cshtml.cs
@@ -0,0 +1,60 @@
+using System.ComponentModel.DataAnnotations;
+using group_finder.Domain.DiscordDomain;
+using group_finder.Domain.Matchmaking;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace group_finder.Pages.Teacher;
+
+public class AddServerModel(ILogger<CreateCourseModel> logger, DiscordService discordService, MatchmakingService matchmakingService) : PageModel
+{
+    private readonly ILogger<CreateCourseModel> _logger = logger;
+    public DiscordServerItem[] Servers = [];
+
+    public string InviteURL = discordService.InviteURL;
+
+    [BindProperty(SupportsGet = true)]
+    public string CourseId { get; set; } = string.Empty;
+
+    public IActionResult OnGet()
+    {
+        var discordIdClaim = User.FindFirst("discordId");
+        if (discordIdClaim == null)
+        {
+            return RedirectToPage("/Login");
+        }
+
+        Load(ulong.Parse(discordIdClaim.Value));
+
+        return Page();
+    }
+
+    public void Load(ulong discordId)
+    {
+
+        Servers = discordService.GetServersOwnedByUser(discordId);
+
+    }
+
+
+    public async Task<IActionResult> OnPostAsync(ulong serverId, Guid courseId)
+    {
+        var discordIdClaim = User.FindFirst("discordId");
+        if (discordIdClaim == null)
+        {
+            return RedirectToPage("/Login");
+        }
+        Load(ulong.Parse(discordIdClaim.Value));
+
+        if (serverId == 0)
+        {
+            return Page();
+        }
+
+        await discordService.EnsureCourseServer(courseId, serverId);
+
+        _logger.LogInformation("Server with ID {ServerId} has been added to course {CourseId}.", serverId, CourseId);
+
+        return RedirectToPage(TeacherRoutes.CourseOverview(), new { id = CourseId });
+    }
+}
