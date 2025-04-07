b625994dbe64e42fd227211ac5c470ff373b771c
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-04-07 15:57:44 +0200

    add day of the week to database

---
+++ Data/Migrations/20250407135324_DayOfTheWeekPref.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickFinder.Migrations
+{
+    /// <inheritdoc />
+    public partial class DayOfTheWeekPref : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Days",
+                table: "Preferences",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Days",
+                table: "CoursePreferences",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Days",
+                table: "Preferences");
+
+            migrationBuilder.DropColumn(
+                name: "Days",
+                table: "CoursePreferences");
+        }
+    }
+}
