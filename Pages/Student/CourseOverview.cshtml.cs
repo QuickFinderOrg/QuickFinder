a9f787f347c9ad8021cbfb2169913da15baaddaf
MartinVorre <116595392+MartinVorre@users.noreply.github.com>
2025-04-11 11:01:19 +0200

    your group in courseoverview

--- Pages/Student/CourseOverview.cshtml.cs
+++ Pages/Student/CourseOverview.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using QuickFinder.Domain.Matchmaking;
@@ -6,7 +7,9 @@ namespace QuickFinder.Pages.Student;
 
 public class CourseOverviewModel(
     ILogger<CourseOverviewModel> logger,
-    CourseRepository courseRepository
+    UserManager<User> userManager,
+    CourseRepository courseRepository,
+    GroupRepository groupRepository
     ) : PageModel
 {
     private readonly ILogger<CourseOverviewModel> _logger = logger;
@@ -14,15 +17,26 @@ public class CourseOverviewModel(
     [BindProperty]
     public Course Course { get; set; } = default!;
 
+    [BindProperty]
+    public Group[] Groups { get; set; } = [];
 
     public async Task<IActionResult> OnGetAsync(Guid courseId)
     {
+        var userId = userManager.GetUserId(User) ?? throw new Exception("User not found");
         var course = await courseRepository.GetByIdAsync(courseId);
         if (course == null)
         {
             return NotFound();
         }
         Course = course;
+
+        var groups = await groupRepository.GetGroups(courseId);
+        groups = [.. groups.Where(g => g.Members.Any(m => m.Id == userId))];
+        if (groups.Length != 0)
+        {
+            Groups = groups;
+        }
+
         return Page();
     }
 }
