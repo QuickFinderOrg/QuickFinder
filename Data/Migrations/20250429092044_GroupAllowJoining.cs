0d6040a30715457ab21fb70c8010796a81c8b6dc
MartinVorre <116595392+MartinVorre@users.noreply.github.com>
2025-04-29 13:20:07 +0200

    allow manual joining migration

---
+++ Data/Migrations/20250429092044_GroupAllowJoining.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickFinder.Migrations
+{
+    /// <inheritdoc />
+    public partial class GroupAllowJoining : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AllowAnyone",
+                table: "Groups",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowAnyone",
+                table: "Groups");
+        }
+    }
+}
