7f2d6fd9db02555bc792e16971794d69010e395c
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2025-05-06 17:07:23 +0200

    migration

---
+++ Data/Migrations/20250506145935_RemoveGlobalPreferences.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickFinder.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveGlobalPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GlobalAvailability",
+                table: "Preferences");
+
+            migrationBuilder.DropColumn(
+                name: "GlobalDays",
+                table: "Preferences");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GlobalAvailability",
+                table: "Preferences",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GlobalDays",
+                table: "Preferences",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
