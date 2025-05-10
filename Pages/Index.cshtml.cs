01470a22b4ddbfa4a6f8b3e80054b229d50bbe4c
MartinVorre <116595392+MartinVorre@users.noreply.github.com>
2025-05-10 13:17:57 +0200

    style the home page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace QuickFinder.Pages;
 
 [Authorize(Policy = "Student")]
-public class IndexModel(ILogger<IndexModel> logger) : PageModel
+public class IndexModel(
+    ILogger<IndexModel> logger,
+    UserManager<User> userManager,
+    UserService userService
+) : PageModel
 {
     private readonly ILogger<IndexModel> _logger = logger;
 
-    public void OnGet() { }
+    public string? Name { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var user =
+            await userManager.GetUserAsync(HttpContext.User)
+            ?? throw new Exception("User not found");
+
+        var name = await userService.GetName(user);
+        if (!string.IsNullOrEmpty(name))
+        {
+            Name = name;
+        }
+
+        return Page();
+    }
 }
